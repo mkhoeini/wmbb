7ddf0383aeaff1237b524a4bc79a63f157230caf
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2025-04-11 14:11:00 +0200

    move db to sss

---
+++ src/sss/db.clj
@@ -0,0 +1,29 @@
+(ns sss.db
+  (:require
+   [datascript.core :as d]
+   [integrant.core :as ig]
+   [sss.log :as log]))
+
+
+
+(defmethod ig/init-key ::db [_ {:keys [schema seed]}]
+  (let [db (d/create-conn schema)]
+    (d/transact! db seed)
+    db))
+
+
+(defn transact [system & tx]
+  (let [tx-data (d/transact! (::db system) tx)]
+    (log/debug "last transaction" tx-data)
+    tx-data))
+
+
+(defn find1 [system & where]
+  (->> @(::db system)
+       (d/q {:find '[?e .] :where where})
+       (d/entity @(::db system))))
+
+(defn find* [system & where]
+  (->> @(::db system)
+       (d/q {:find '[[?e ...]] :where where})
+       (map #(d/entity @(::db system) %))))
