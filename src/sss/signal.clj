b54fa2fd4bef11de6b8cfdb205ffa4a4cb900892
Mohammad Sadegh Khoeini <mohammadk@spotify.com>
2025-04-21 15:17:59 +0200

    feat: reimplement protocol based implementation of sss.signal to data based

--- src/sss/signal.clj
+++ src/sss/signal.clj
@@ -6,28 +6,34 @@
 
 
 
-(defprotocol SignalsState
-  (-get-signals [this])
-  (-get-signal-chan [this signal])
-  (-get-signal-mult [this signal])
-  (-send-signal! [this signal data])
-  (-subscribe! [this signal chan]))
-
-
 (defmethod ig/init-key ::signals [_ {:keys [buf-fn db-conn] {:keys [signals]} :cfg}]
   (apply db/transact! db-conn
          (for [s signals] {::name s}))
   (let [chans (into {} (for [s signals] [s (async/chan (buf-fn))]))
         mults (into {} (for [s signals :let [ch (chans s)]] [s (async/mult ch)]))]
-    (reify SignalsState
-      (-get-signals [_] signals)
-      (-get-signal-chan [_ sig] (chans sig))
-      (-get-signal-mult [_ sig] (mults sig))
-      (-send-signal! [_ signal data]
-        (let [ch (chans signal)]
-          (async/put! ch data)))
-      (-subscribe! [_ signal ch]
-        (async/tap (mults signal) ch)))))
+    {:signals signals
+     :chans chans
+     :mults mults}))
+
+
+(defn -get-signals [events-state]
+  (:signals events-state))
+
+
+(defn -get-signal-chan [events-state signal]
+  (get-in events-state [:chans signal]))
+
+
+(defn -get-signal-mult [events-state signal]
+  (get-in events-state [:mults signal]))
+
+
+(defn -send-signal! [events-state signal data]
+  (async/put! (-get-signal-chan events-state signal) data))
+
+
+(defn -subscribe! [events-state signal chan]
+  (async/tap (-get-signal-mult events-state signal) chan))
 
 
 (defmethod ig/halt-key! ::signals [_ sigs]
@@ -40,7 +46,3 @@
 (defn send-signal! [system signal data]
   (let [data-with-meta (vary-meta data assoc :sss/system system)]
     (-send-signal! (::signals system) signal data-with-meta)))
-
-
-(defn subscribe! [system signal chan]
-  (-subscribe! (::signals system) signal chan))
